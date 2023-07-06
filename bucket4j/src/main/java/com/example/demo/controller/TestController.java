ac6f88331d799e68e5b6a6eecff701f26522515f
bohub12 <qhrb1997@naver.com>
2023-07-07 01:44:12 +0900

    feat: rate limiter using bucket4j

--- bucket4j/src/main/java/com/example/demo/controller/TestController.java
+++ bucket4j/src/main/java/com/example/demo/controller/TestController.java
@@ -1,21 +1,35 @@
 package com.example.demo.controller;
 
+import com.example.demo.price.PricingPlanService;
 import io.github.bucket4j.Bucket;
+import io.github.bucket4j.ConsumptionProbe;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class TestController {
 
-    @GetMapping("/test")
-    public ResponseEntity<?> test() {
-        // TODO = IP 주소별로 버킷 생성 및 토큰 삭제
+    private final PricingPlanService pricingPlanService;
 
-        return ResponseEntity.ok().build();
+    @GetMapping("/test")
+    public ResponseEntity<?> test(@RequestHeader(value = "x-api-key") String apiKey) {
+        Bucket bucket = pricingPlanService.resolveBucket(apiKey);
+        ConsumptionProbe probe = bucket.tryConsumeAndReturnRemaining(1);
+
+        if (probe.isConsumed()) {
+            return ResponseEntity.ok()
+                    .header("X-Rate-Limit-Remaining", Long.toString(probe.getRemainingTokens()))
+                    .body("ok");
+        }
+
+        long waitForRefill = probe.getNanosToWaitForRefill() / 1_000_000_000;
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                .header("X-Rate-Limit-Retry-After-Seconds", String.valueOf(waitForRefill))
+                .build();
+
     }
 }
