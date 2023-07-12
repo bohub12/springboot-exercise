4e9807b823f0fb7793a58b3ff69d20f82d9750e6
bohub12 <qhrb1997@naver.com>
2023-07-13 01:25:57 +0900

    feat: member argument resolver

---
+++ spring-mvc/argument-resolver/src/main/java/com/example/demo/controller/MemberController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import com.example.demo.domain.Member;
+import com.example.demo.jwt.JwtUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/member")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final JwtUtils jwtUtils;
+
+    @GetMapping("")
+    public ResponseEntity<?> get(Member member) {
+        return ResponseEntity.ok().body(member.toString());
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> save(@RequestParam("id") Long id, @RequestParam("name") String name) {
+        return ResponseEntity.ok().body(jwtUtils.createToken(id, name));
+    }
+}
--- spring-mvc/argument-resolver/src/main/java/com/example/demo/domain/Member.java
+++ spring-mvc/argument-resolver/src/main/java/com/example/demo/domain/Member.java
@@ -1,7 +1,11 @@
 package com.example.demo.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
 public class Member {
     private Long id;
     private String name;
-    private Food favoriteFood;
 }
